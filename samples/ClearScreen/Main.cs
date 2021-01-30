1966730271733bcf7b7a97d4f9e32cd779447e9d
Diogo Andrade <diogo.g.andrade@gmail.com>
2021-01-30 20:09:23 +0000

    Reorganizing project files (WIP, broken)

---
+++ samples/ClearScreen/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SoftRender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application app = new Samples.ClearScreen();
+
+            if (!app.Run())
+            {
+                Debug.Log("Failed to run application!");
+            }
+        }
+    }
+}
---
+++ samples/SineWaves/Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SoftRender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//            Application app = new Samples.ClearScreen();
+//            Application app = new Samples.SineWaves();
+//            Application app = new Samples.UnclippedLines();
+//              Application app = new Samples.ClippedLines();
+//            Application app = new Samples.RandomLines();
+            Application app = new Samples.Fireworks();
+
+
+            if (!app.Run())
+            {
+                Debug.Log("Failed to run application!");
+            }
+        }
+    }
+}
